021d42c602f1d07721e2594b76a53ad5af102bb1
Usuario <Usuario@Usuario-PC>
2022-06-09 14:50:54 -0500

    Taller 02 ingeniería de software

---
+++ proyecto02_Ing_sw/src/proyecto02_ing_sw/Proyecto02_Ing_sw.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02_ing_sw;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Proyecto02_Ing_sw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
